7c445692c76ab32b20c9cf4609745e8bb947a286
Tran Duong Quang <44544391+duongquang18it3@users.noreply.github.com>
2024-04-11 17:08:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,16 +92,24 @@ if authentication_status:
                 
         else:
             st.sidebar.info("You have not been assigned to any projects.")
+
+    def show_logout_button():
+        if st.sidebar.button('Log out'):
+            st.session_state['logged_in'] = False
+            if 'username' in st.session_state:
+                del st.session_state['username']
+            st.experimental_rerun() 
+
+    def main():
+        if 'logged_in' not in st.session_state:
+            st.session_state['logged_in'] = False
+
+        if st.session_state['logged_in']:
+            show_logout_button()
+
+    if __name__ == "__main__":
+        main()
 else:
     st.error("Username/password is incorrect")
 
 
-
-def main():
-    if 'logged_in' not in st.session_state:
-        st.session_state['logged_in'] = False
-
-
-
-if __name__ == "__main__":
-    main()
