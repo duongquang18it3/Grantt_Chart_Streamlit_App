64d88fcae2b29a534963e3cdee84939d7b15056f
Tran Duong Quang <44544391+duongquang18it3@users.noreply.github.com>
2024-04-26 16:42:52 +0700

    Update Connect_DMS_API.py

--- pages/Connect_DMS_API.py
+++ pages/Connect_DMS_API.py
@@ -5,22 +5,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import numpy as np
 st.set_page_config(layout="wide")
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def add_custom_css():
-    # Đoạn CSS để tùy chỉnh màu nền của các cột
-    st.markdown("""
-        <style>
-        .st-cj {
-            background-color: #f0f2f6;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
-# Gọi hàm để thêm CSS
-add_custom_css()
 
 # URLs for the data
 urls = {
@@ -52,58 +37,77 @@ def fetch_data(endpoint_url):
 # Sidebar for selecting the view
 option = st.sidebar.selectbox(
     "Select Dashboard View",
-    ("Document Type", "Document Growth Over Time", "Cabinet Document Distribution", "Document Tags", "Document Count by Index and Node Value")
+    ("Document Type",  "Cabinet Document Distribution", "Document Tags", "Document Count by Index and Node Value")
 )
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 
 # Main dashboard layout based on sidebar selection
 # Depending on the option, display appropriate visuals
 if option == "Document Type":
     st.header('Document Types')
     df_documents = pd.DataFrame(fetch_data(urls['documents']))
+    df_documents['type_label'] = df_documents['document_type'].apply(lambda x: x.get('label') if isinstance(x, dict) else None)
+    unique_document_types_count = df_documents['type_label'].nunique()
     if not df_documents.empty:
         # Document Type Charts
         # [Include your visualization code for Document Types here]
-        col1, col2 = st.columns([6.5, 3.5])
+        col1, col2, col3 = st.columns([1.5, 4.5, 4], gap='small')
+        total_documents_count = df_documents.shape[0]
+# Tính toán tổng số loại tài liệu (loại tài liệu duy nhất)
+        
         with col1:
-            df_documents['type_label'] = df_documents['document_type'].apply(lambda x: x.get('label') if isinstance(x, dict) else None)
+            st.markdown('##### Statistics')
+            st.metric(label="Total Documents", value=f"{total_documents_count}")
+            st.metric(label="Total Document Types", value=f"{unique_document_types_count}")
+        with col2:
+            
             type_counts = df_documents['type_label'].value_counts().reset_index()
             type_counts.columns = ['type_label', 'count']
             total_documents = type_counts['count'].sum()
             type_counts['percentage'] = (type_counts['count'] / total_documents) * 100
 
             # Layout with two columns
-            col11, col22 = st.columns([3, 7])
-
-            with col11:
-               
-                st.markdown("**Select Document Types Percentage Range**")
-                min_percentage = st.number_input('Enter minimum percentage', min_value=0.0, max_value=100.0, value=0.0, step=0.1)
-                max_percentage = st.number_input('Enter maximum percentage', min_value=0.0, max_value=100.0, value=100.0, step=0.1)
             
+
+            
+               
+            st.markdown('##### Select Document Types Percentage Range')
+
+            col11, col22,col33 = st.columns([4,4,2])
+            with col11:
+                min_percentage = st.number_input('Enter minimum percentage', min_value=0.0, max_value=100.0, value=0.0, step=0.1)
+                
             with col22:
+                max_percentage = st.number_input('Enter maximum percentage', min_value=0.0, max_value=100.0, value=100.0, step=0.1)
                 # Validate the range and plot the pie chart if valid
-                if min_percentage < max_percentage:
+            if min_percentage < max_percentage:
                     filtered_data = type_counts[(type_counts['percentage'] >= min_percentage) & (type_counts['percentage'] <= max_percentage)]
                     fig_pie = px.pie(filtered_data, names='type_label', values='count',width=450,
                                     title=f'Pie Chart of Document Types from {min_percentage}% to {max_percentage}%')
                     st.plotly_chart(fig_pie)
-                else:
+            else:
                     st.error('Minimum percentage must be less than maximum percentage.')
-        with col2:
+        with col3:
+            st.markdown('##### Popularity of Document Types by Label')
             fig_bubble = px.scatter(type_counts, x='type_label', y='count',
                             size='count', color='type_label',
-                            hover_name='type_label', size_max=60,width=400,
-                            title='Popularity of Document Types by Label')
+                            hover_name='type_label', size_max=40,width=450)
             st.plotly_chart(fig_bubble)
 
-        col1, col2 = st.columns([6.5, 3.5])
-        with col1:
-            fig_bar = px.bar(type_counts, x='count', y='type_label', color='type_label',
+        
+        st.markdown('##### Bar Chart of Document Types')
+        fig_bar = px.bar(type_counts, x='count', y='type_label', color='type_label',
                      labels={'type_label': 'Document Type', 'count': 'Count'},
-                     title='Bar Chart of Document Types', text='count')
-            fig_bar.update_traces(texttemplate='%{text}', textposition='outside')
-            st.plotly_chart(fig_bar)
-        with col2:
+                      text='count', width=1000,height=450)
+        fig_bar.update_traces(texttemplate='%{text}', textposition='outside')
+        st.plotly_chart(fig_bar)
+
+
+        col1, col2 = st.columns([6, 4], gap="medium")
+        with col1:
+            st.markdown('##### Document File Extensions')
             df_documents['file_extension'] = df_documents['file_latest'].apply(lambda x: x['mimetype'].split('/')[-1].upper() if isinstance(x, dict) else None)
 
             # Count the frequency of each file extension
@@ -112,44 +116,108 @@ if option == "Document Type":
 
             # Create a Bar Chart
             fig_file_extension = px.bar(extension_counts, x='File Extension', y='Count', color='File Extension',
-                                    title='Document File Extensions', width=400,text='Count')
+                                     text='Count', width=600)
             fig_file_extension.update_traces(texttemplate='%{text}', textposition='outside')
             fig_file_extension.update_layout(bargap=0.5)
             st.plotly_chart(fig_file_extension)
 
-elif option == "Document Growth Over Time":
+        with col2:
+            st.markdown('##### Top documents')
+
+            # Fetch data from the "documents" API
+            df_documents = pd.DataFrame(fetch_data(urls['documents']))
+
+            # Calculate document type counts based on the 'label' key
+            type_counts = df_documents['document_type'].apply(lambda x: x.get('label') if isinstance(x, dict) else None).value_counts().reset_index()
+            type_counts.columns = ['Document Type', 'Count']
+
+            # Sort and filter top documents
+            top_documents_df = type_counts.sort_values(by='Count', ascending=False).head(10)
+
+            # Check if DataFrame is empty before calculating max_count
+            if not top_documents_df.empty:
+                max_count = top_documents_df['Count'].max()
+            else:
+                max_count = 0  # Or any default value for an empty DataFrame
+
+            # Use st.data_editor to display the table with document type labels
+            st.data_editor(
+                top_documents_df,
+                column_order=("Document Type", "Count"),
+                column_config={
+                    "Document Type": st.column_config.TextColumn(
+                        "Document Types",  # Set the header text as "Document Types"
+                    ),
+                    "Count": st.column_config.ProgressColumn(
+                        "Number of Documents",
+                        format="%f",  # Display format for count
+                        min_value=float(0),
+                        max_value=float(max_count),
+                    ),
+                },
+                hide_index=True,  # Hide the index column
+            )
+        
     st.header('Document Growth Over Time')
     df_documents = pd.DataFrame(fetch_data(urls['documents']))
     if not df_documents.empty:
             #Document Growth Over Time:
         # Convert datetime_created to datetime and extract date part
         df_documents['date'] = pd.to_datetime(df_documents['datetime_created']).dt.date
-        df_documents['document_type'] = df_documents['document_type'].apply(lambda x: x['label'])
-        df_documents['file_extension'] = df_documents['file_latest'].apply(lambda x: x['mimetype'].split('/')[-1].upper())
+        df_documents['document_type'] = df_documents['document_type'].apply(lambda x: x.get('label') if isinstance(x, dict) else None)
+        df_documents['file_extension'] = df_documents['file_latest'].apply(lambda x: x['mimetype'].split('/')[-1].upper() if isinstance(x, dict) else None)
 
-        # Group by date and count by document type or file extension
-        def prepare_data(group_field):
-            df_grouped = df_documents.groupby(['date', group_field]).size().reset_index(name='count')
-            return df_grouped.pivot(index='date', columns=group_field, values='count').fillna(0).cumsum().reset_index()
+        # Determine the earliest and latest dates in the dataset
+        min_date = df_documents['date'].min()
+        max_date = df_documents['date'].max()
 
-        # User selection for data view
-        option = st.selectbox('Choose Data View', ['Document Types Growth Over Time', 'File Extension Growth Over Time'])
+        # User input for date range
+        with st.sidebar:
+            start_date = st.date_input("Start date", value=min_date, min_value=min_date, max_value=max_date)
+            end_date = st.date_input("End date", value=max_date, min_value=min_date, max_value=max_date)
 
-        if option == 'Document Types Growth Over Time':
-            df_plot = prepare_data('document_type')
-            title = "Document Type Growth Over Time"
-        else:
-            df_plot = prepare_data('file_extension')
-            title = "File Extension Growth Over Time"
+        # Filtering data based on date range
+        filtered_documents = df_documents[(df_documents['date'] >= start_date) & (df_documents['date'] <= end_date)]
+        
+        col1, col2 = st.columns([4,6])
+        with col1:
+        # Display document details in a table
+            st.write("Filtered Documents:", filtered_documents[['label', 'document_type']])
 
-        # Plotting the data
-        fig = px.line(df_plot, x='date', y=[col for col in df_plot.columns if col != 'date'],
-                    labels={'value': 'Number of Documents', 'date': 'Date'},
-                    title=title)
-        fig.update_layout(xaxis_title='Date', yaxis_title='Cumulative Document Count', legend_title=option[:-16])
-        st.plotly_chart(fig)
-            # Document Growth Charts
-            # [Include your visualization code for Document Growth Over Time here]
+        # Choose data view and plot
+        with col2:
+            option = st.selectbox('Choose Data View', ['Document Types Growth Over Time', 'File Extension Growth Over Time'])
+
+            def prepare_data(df, group_field):
+                df_grouped = df.groupby(['date', group_field]).size().reset_index(name='count')
+                df_pivot = df_grouped.pivot(index='date', columns=group_field, values='count').fillna(0).cumsum().reset_index()
+                return df_pivot
+
+            if option == 'Document Types Growth Over Time':
+                df_plot = prepare_data(filtered_documents, 'document_type')
+                title = "Document Type Growth Over Time"
+            else:
+                df_plot = prepare_data(filtered_documents, 'file_extension')
+                title = "File Extension Growth Over Time"
+
+            # Plotting
+            if not df_plot.empty:
+                fig = px.line(df_plot, x='date', y=[col for col in df_plot.columns if col != 'date'],
+                            labels={'value': 'Number of Documents', 'date': 'Date'},
+                            title=title)
+                fig.update_layout(xaxis_title='Date', yaxis_title='Cumulative Document Count', legend_title=option[:-16])
+                st.plotly_chart(fig)
+            else:
+                st.write("No data to display for the selected range.")
+
+       
+           
+            
+
+
+
+
+
 
 elif option == "Cabinet Document Distribution":
     st.header('Cabinets')
@@ -203,7 +271,7 @@ elif option == "Cabinet Document Distribution":
 
             # Treemap with document counts
             fig_cabinets = px.treemap(df_cabinet_documents, path=[px.Constant("All Cabinets"), 'parent', 'label'], values='document_count',
-                                    title="Cabinet Document Distribution", hover_data={'label': True, 'document_count': True})
+                                    title="Cabinet Document Distribution", hover_data={'label': True, 'document_count': True}, height=600, width=900)
             st.plotly_chart(fig_cabinets)
         # Cabinet Document Distribution Charts
         # [Include your Treemap visualization here]
@@ -232,7 +300,7 @@ elif option == "Document Tags":
             df_tags['color'] = df_tags.apply(lambda x: x['color'], axis=1)
 
             # Bar Chart for Tags
-            fig_tags = px.bar(df_tags, x='label', y='document_count', title="Documents by Tag",
+            fig_tags = px.bar(df_tags, x='label', y='document_count', title="Documents by Tag",height=600, width=900,
                             color='color', text='document_count')
             fig_tags.update_layout(showlegend=False)  # Optional: Turn off the legend if color coding is sufficient
             st.plotly_chart(fig_tags)
@@ -292,6 +360,6 @@ elif option == "Document Count by Index and Node Value":
 
     # Create a stacked bar chart
     
-    fig = px.bar(df_node_counts, x='Index Label', y='Document Count', color='Node Value', text='Document Count', barmode='stack')
+    fig = px.bar(df_node_counts, x='Index Label', y='Document Count', color='Node Value', text='Document Count', height=600,width=900, barmode='stack')
     fig.update_traces(texttemplate='%{text}', textposition='outside')
     st.plotly_chart(fig)
